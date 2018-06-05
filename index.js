5f2269c39c413312d080de7f9a9c3d90c38490db
Leo Trudel <leoltrudel@gmail.com>
2018-06-05 23:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   return empty
 }
 
+<<<<<<< HEAD
 function johnLennonFacts(facts) {
   var results = [];
   var i = 0;
@@ -24,3 +25,15 @@ function iLoveTheBeatles(number) {
   } while (number <= 14)
   return empty
 }
+=======
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newFacts;
+}
+>>>>>>> 2990f2771f56b5d973ce9fd2ec012b075c6b1bed
